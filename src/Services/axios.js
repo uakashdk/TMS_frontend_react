fdea0b91c13b8111a8d583aea87bc501c0f84011
uakashdk <akashshaurya1998@gmail.com>
2026-04-07 17:02:03 +0530

    axios setup

--- src/Services/axios.js
+++ src/Services/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1/fleetlio",
+  // baseURL: "http://localhost:5000/api/v1/fleetlio",
+  baseURL:"http://15.134.220.228/api/v1/fleetlio",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
